f2e5ecabf7a20c475f19a977c178f48c564b9564
FRANCO NICOLAS CZABAN DIAZ <francoczaban@gmail.com>
2024-11-15 15:01:54 -0300

    paginador, filtro y sort

--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -1,36 +1,62 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { TableColumn } from '../../models/table-column';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   standalone: true,
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
-  imports: [MatTableModule, CommonModule]
+  imports: [MatTableModule, MatPaginatorModule, CommonModule, MatInputModule, MatFormFieldModule, MatSortModule]
 })
-export class TableComponent implements OnInit {
-
-  dataSource: any[string];
-  displayedColumns: any = [];
+export class TableComponent implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = [];
   tableColumns: TableColumn[] = [];
-  filteredDataSource: any[] = [];
 
-  @Input() set data(data: any) {
-    this.dataSource = data;
+  @Input() set data(data: any[]) {
+    this.dataSource.data = data;
   }
 
   @Input() set columns(columns: TableColumn[]) {
-    this.tableColumns = columns
-    this.displayedColumns = this.tableColumns.map(col => col.def)
+    this.tableColumns = columns;
+    this.displayedColumns = this.tableColumns.map(col => col.def);
   }
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;  // Agregar el ViewChild para MatSort
 
   ngOnInit(): void {}
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;  // Asignar MatSort al dataSource
+  }
+
+  isActionColumn(column: TableColumn, row: any): boolean {
+    return column.def === 'accion' &&
+      row.sincronizar === 'No' &&
+      row.rol === 'Learner';
+  }
+
   isArray(value: any): boolean {
     return Array.isArray(value);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
