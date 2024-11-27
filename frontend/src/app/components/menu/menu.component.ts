cadc189f80e46c8989217ba3d8550c37908830cf
ottognr <alemendive@gmail.com>
2024-11-26 21:35:10 -0300

    fix del import y cambiando menu a navbar con sesion a la der

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,40 +1,19 @@
 import { Component } from '@angular/core';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, RouterModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
+  constructor(private router: Router, private authService: AuthService) {}
 
-
-  constructor(private router: Router, private authService: AuthService) { }
-
-  aMateriales() {
-    this.router.navigateByUrl("/materiales");
+  navigateTo(path: string) {
+    this.router.navigateByUrl(path);
   }
-
-  aMaterialesCompuestos() {
-    this.router.navigateByUrl("/materiales-compuestos");
-  }
-
-  aListado() {
-    this.router.navigateByUrl("/listado");
-  }
-
-  aProductos() {
-    this.router.navigateByUrl("/productos");
-  }
-
-  aProveedores() {
-    this.router.navigateByUrl("/proveedores");
-    }
 
   logout() {
     this.authService.logout();
