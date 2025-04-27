99049e25fe1380ad1c915c6c8b72e72a5ceb79ae
FRANCO NICOLAS CZABAN DIAZ <francoczaban@gmail.com>
2025-04-27 12:58:59 -0300

    estilos menu y material compuesto form

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { NotificationsComponent } from '../notifications/notifications.component'
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
-  imports: [NotificationsComponent],
+  imports: [NotificationsComponent,MatIconModule],
 })
 export class MenuComponent {
   constructor(private router: Router, private authService: AuthService) {}
